25e9fd712392be5ec43e67905f5c6f628c372bc5
novitaguok <novitaguok@gmail.com>
2025-01-23 08:35:26 +0700

    Init project

--- internal/model/request/LoginReq.go
+++
@@ -1,6 +0,0 @@
-package request
-
-type LoginReq struct {
-	Email    string `json:"username" binding:"required, email"`
-	Password string `json:"password" binding:"required, min=8"`
-}
--- internal/model/request/RegisterReq.go
+++
@@ -1,6 +0,0 @@
-package request
-
-type RegisterReq struct {
-	Email    string `json:"username" binding:"required, email"`
-	Password string `json:"password" binding:"required, min=8"`
-}
